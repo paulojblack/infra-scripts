05e441bcc6f2f2ebf09703b5ea8f8ae3a170e225
Paulo Black <paulo@pauloblack.com>
2023-04-20 10:43:02 -0400

    omfg it works

--- crespira.tf
+++ crespira.tf
@@ -22,6 +22,7 @@ resource "aws_ecr_repository" "api_srv" {
 ###
 ### NETWORK CONFIGURATION
 ###
+
 resource "aws_vpc" "default_vpc" {
   cidr_block = "172.31.0.0/16"
 
@@ -76,6 +77,7 @@ resource "aws_security_group_rule" "allow_all_out" {
 ###
 ### CLOUDWATCH LOG GROUPS
 ###
+
 resource "aws_cloudwatch_log_group" "crespira_web_log_group" {
   name = "crespira-web"
   retention_in_days = 14
@@ -112,7 +114,7 @@ resource "aws_ecs_task_definition" "crespira" {
     logConfiguration = {
         logDriver = "awslogs"
         options = {
-          "awslogs-group" = "aws_cloudwatch_log_group.crespira_web_log_group.name"
+          "awslogs-group" = "crespira-web"
           "awslogs-region" = "us-east-2"
           "awslogs-stream-prefix" = "crespira-web"
         }
@@ -131,56 +133,13 @@ resource "aws_ecs_task_definition" "crespira" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        "awslogs-group" = "aws_cloudwatch_log_group.crespira_api_log_group.name"
+        "awslogs-group" = "crespira-api"
         "awslogs-region" = "us-east-2"
         "awslogs-stream-prefix" = "crespira-api"
       }
     }
   }])
 }
-# resource "aws_ecs_task_definition" "crespira-web_task" {
-#   family                = "crespira-web"
-#   requires_compatibilities = ["FARGATE"]
-#   network_mode         = "awsvpc"
-#   cpu                  = "256"
-#   memory               = "512"
-#   execution_role_arn   = aws_iam_role.ecs_execution_role.arn
-#   task_role_arn        = aws_iam_role.ecs_task_role.arn
-
-#   container_definitions = jsonencode([{
-#     name  = "crespira-web"
-#     image = local.ui_image_url
-#     essential = true
-#     portMappings = [
-#       {
-#         containerPort = 80
-#         hostPort      = 80
-#       }
-#     ]
-#   }])
-# }
-
-# resource "aws_ecs_task_definition" "crespira-api_task" {
-#   family                = "crespira-api"
-#   requires_compatibilities = ["FARGATE"]
-#   network_mode         = "awsvpc"
-#   cpu                  = "256"
-#   memory               = "512"
-#   execution_role_arn   = aws_iam_role.ecs_execution_role.arn
-#   task_role_arn        = aws_iam_role.ecs_task_role.arn
-
-#   container_definitions = jsonencode([{
-#     name  = "crespira-api"
-#     image = local.api_image_url
-#     essential = true
-#     portMappings = [
-#       {
-#         containerPort = 3001
-#         hostPort      = 3001
-#       }
-#     ]
-#   }])
-# }
 
 ###
 ### ECS SERVICE CONFIGURATION
@@ -201,23 +160,6 @@ resource "aws_ecs_service" "crespira" {
 
   depends_on = [aws_ecs_task_definition.crespira]
 }
-
-# resource "aws_ecs_service" "crespira-api" {
-#   name            = "crespira-api"
-#   cluster         = aws_ecs_cluster.this.id
-#   task_definition = aws_ecs_task_definition.crespira-api_task.arn
-#   desired_count   = 1
-#   launch_type     = "FARGATE"
-
-#   network_configuration {
-#     subnets          = aws_subnet.private.*.id
-#     security_groups  = [aws_security_group.allow_all.id]
-#     assign_public_ip = true
-#   }
-
-#   depends_on = [aws_ecs_task_definition.crespira-api_task]
-# }
-
 
 ###
 ### PERMISSIONS CONFIGURATION
